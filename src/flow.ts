74b348877c5638686647d93373ab9dc21330a2ce
lesguillemets <lesguillemets@users.noreply.github.com>
2015-11-06 23:29:32 +0900

    keyHandler

--- src/flow.ts
+++ src/flow.ts
@@ -177,6 +177,51 @@ function spawnN (c: SpawnConfig, n: number) : Dot[] {
 
 // }}}
 
+// keyhander.hs {{{
+var baseAccel = 5;
+// what I want is basically a Set (of int).
+function addKeyHandler(pressed: boolean[]) : void{
+  // keep track of what's being pressed.
+  document.addEventListener('keydown', function(e){
+    pressed[e.keyCode] = true;
+  })
+  document.addEventListener('keyup', function(e){
+    pressed[e.keyCode] = false;
+  })
+}
+
+// TODO : reconsider implementation.
+// Probably it's better if we just use Array or Obj.
+function keyConfig(keyCode:number) : Vector {
+  switch(keyCode){
+    case 65: // a
+    case 37: // Left
+      return (new Vector(-baseAccel,0)); // left
+    case 87: // w
+    case 38: // Up
+      return (new Vector(0, -baseAccel)); // up
+    case 68: // d
+    case 39: // Right
+      return (new Vector(baseAccel,0)); // right
+    case 83:
+    case 40:
+      return (new Vector(0,baseAccel)); // down
+    default:
+      return (new Vector(0,0));
+  }
+}
+
+// given a list of pressed keys, returns the acceleration.
+function fromPressed(pressed: boolean[]) : Vector {
+  return (
+    pressed
+      .map((pr,i) => {return (keyConfig(i)); })
+      .reduce(((acc, v) => {return vAdd(acc,v)}), new Vector(0,0))
+      );
+}
+
+// }}}
+
 function main() : void {
   console.log('hi');
   var n : Vector = new Vector(4,5);
